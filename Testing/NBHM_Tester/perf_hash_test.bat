672577a692671bc4939325b44b556a55ab3ccb4f
Cliff Click <cliff@azulsystems.com>
2008-11-04 07:57:08 +0000

    version 1.0

---
+++ Testing/NBHM_Tester/perf_hash_test.bat
@@ -0,0 +1,8 @@
+javac -classpath "c:\Documents and Settings\Cliff\Desktop\Highly Scalable Java\high-scale-lib"   NBHM_Tester2.java
+java  -classpath "c:\Documents and Settings\Cliff\Desktop\Highly Scalable Java\high-scale-lib;." NBHM_Tester2
+
+rem javac -classpath "c:\Documents and Settings\Cliff\Desktop\Highly Scalable Java\high-scale-lib" perf_hash_test.java
+rem java  -classpath "c:\Documents and Settings\Cliff\Desktop\Highly Scalable Java\high-scale-lib;." -Xmx512m perf_hash_test 95 1 4 1 1000 6
+
+rem javac -classpath "c:\Documents and Settings\Cliff\Desktop\Highly Scalable Java\high-scale-lib" perf_hashlong_test.java
+rem java  -classpath "c:\Documents and Settings\Cliff\Desktop\Highly Scalable Java\high-scale-lib;." -Xmx512m perf_hashlong_test 95 1 4 1 1000
---
+++ prj.el
@@ -0,0 +1,9 @@
+(jde-project-file-version "1.0")
+(jde-set-variables
+ '(jde-log-max 5000)
+ '(jde-run-executable-args nil)
+ '(jde-resolve-relative-paths-p t)
+ '(jde-sourcepath (quote ("c:/cliffc/Highly_Scalable_Java/high-scale-lib/org/cliffc/high_scale_lib/" "c:/cliffc/Highly_Scalable_Java/high-scale-lib/Testing/NBHM_Tester/" "c:/cliffc/Highly_Scalable_Java/high-scale-lib/Testing/NBHS_Tester/")))
+ '(jde-run-option-hotspot-type (quote client))
+ '(jde-compile-option-debug (quote ("all" (t nil nil))))
+ '(jde-compile-option-command-line-args nil))
